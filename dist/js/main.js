10251098f1667c5d3ad4a6da63406cff79ea64ae
Erin Kahn <erinnicolekahn@gmail.com>
2018-05-20 19:43:52 -0400

    some animation
    need help on positioning elements...UGH!

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,4 +6,11 @@ document.querySelector('.logo img').addEventListener('click', function () {
     //show the vertical line menu
     document.querySelector('.vertical-line').classList.toggle('show');
 });
+
+document.querySelector('.mobile-menu img').addEventListener('click', function () {
+    //show the mobile menu
+    document.querySelector('.nav').classList.toggle('show');
+    TweenMax.staggerFrom('.nav ul li', 1.5, { opacity: 0, x: -1000 }, 0.5);
+    TweenMax.staggerTo('.nav ul li', 1.5, { opacity: 1 }, 0.5);
+});
 //# sourceMappingURL=main.js.map
