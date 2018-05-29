7327ee72f5d46aaf5066a7e47b16d43678ab4349
Erin Kahn <erinnicolekahn@gmail.com>
2018-05-29 18:42:51 -0400

    trying to loop through gallery images

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,4 +13,41 @@ document.querySelector('.mobile-menu img').addEventListener('click', function ()
     document.querySelector('.nav').classList.toggle('show');
     TweenMax.staggerTo('.nav ul li', 1.5, { opacity: 1 }, 0.5);
 });
+
+// ------------------------------------------------------------------------------------------
+
+
+// gallery images
+
+
+//when you click interior, it shows you all interior images and hides exterior images
+document.querySelector('.extAndInt h4:first-child').addEventListener('click', function () {
+
+    document.querySelector('.interior.gallerySlide').style.display = 'flex';
+    document.querySelector('.exterior.gallerySlide').style.display = 'none';
+
+    var interiorDots = document.querySelectorAll('.slideCircles div');
+    var interiorImages = document.querySelectorAll('.interior.gallerySlide img');
+    var currentDot = 0;
+    var currentImage = 0;
+
+    //loop through dots and images
+    for (var i = 0; i < interiorDots.length; i++) {
+        interiorDots[i].addEventListener('click', function () {});
+        dot[i].addEventListener('click', function () {
+            for (var _i = 0; _i < interiorImages.length; _i++) {
+                interiorImages[_i].classList.remove('active');
+            }
+        });
+    }
+});
+
+//when you click exterior, it shows you all exterior images and hide interior images
+document.querySelector('.extAndInt h4:last-child').addEventListener('click', function () {
+
+    document.querySelector('.exterior.gallerySlide').style.display = 'flex';
+    document.querySelector('.interior.gallerySlide').style.display = 'none';
+
+    //loop through dots and images
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,50 @@ document.querySelector('.mobile-menu img').addEventListener('click', function(){
     document.querySelector('.nav').classList.toggle('show');
     TweenMax.staggerTo('.nav ul li', 1.5, {opacity: 1}, 0.5);
 })
+
+
+
+// ------------------------------------------------------------------------------------------
+
+
+// gallery images
+
+
+
+
+
+//when you click interior, it shows you all interior images and hides exterior images
+document.querySelector('.extAndInt h4:first-child').addEventListener('click', function(){
+
+    document.querySelector('.interior.gallerySlide').style.display = 'flex';
+    document.querySelector('.exterior.gallerySlide').style.display = 'none';
+
+    let interiorDots = document.querySelectorAll('.slideCircles div');
+    let interiorImages = document.querySelectorAll('.interior.gallerySlide img');
+    let currentDot = 0;
+    let currentImage = 0;
+
+    //loop through dots and images
+    for (let i = 0; i < interiorDots.length; i++) {
+        interiorDots[i].addEventListener('click', function() {
+            for (let i = 0; i < interiorImages.length; i++){
+                interiorImages[i].classList.add('active');
+            }
+        }) 
+            
+     })
+    }
+      
+})
+
+//when you click exterior, it shows you all exterior images and hide interior images
+document.querySelector('.extAndInt h4:last-child').addEventListener('click', function(){
+
+    document.querySelector('.exterior.gallerySlide').style.display = 'flex';
+    document.querySelector('.interior.gallerySlide').style.display = 'none';
+
+    //loop through dots and images
+})
+
+
+
